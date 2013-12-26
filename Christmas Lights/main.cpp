3538a87c80c4a15a153f39ea68d10656f1c8a468
Jeremy Pry <jeremy.pry@gmail.com>
2013-12-25 20:23:37 -0500

    Initial Commit

---
+++ Christmas Lights/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Christmas Lights
+//
+//  Created by Jeremy Pry on 12/25/13.
+//  Copyright (c) 2013 Jeremy Pry. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
